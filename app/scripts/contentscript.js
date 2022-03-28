3b336076199b115f1252e2e192fc2509b1c6c25e
Hiroki Miyashita <miyashita_hiroki@tenso.com>
2022-03-28 17:24:32 +0900

    add: commitNumberLimit

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -6,7 +6,7 @@
     let issueTitle = container.querySelector('.js-issue-title').textContent;
     let targetBranch = container.querySelector('span.commit-ref.css-truncate.user-select-contain.expandable.base-ref > a > span').textContent;
     let repositoryName = container.querySelector('h1 > strong > a').textContent;
-    let commitNumber = container.querySelector('#commits_tab_counter').textContent;
+    let commitNumber = Number(container.querySelector('#commits_tab_counter').textContent);
     let buttonMerges = container.querySelectorAll('.merge-message button[data-details-container]');
     let buttonMergeOptions = container.querySelectorAll('.merge-message button[data-details-container] + .select-menu-button');
     let disabled = false;
@@ -18,8 +18,8 @@
       let localStorage = response.localStorage;
       const wipTitleRegex = /[\[(^](do\s*n[o']?t\s*merge|wip|dnm)[\]):]/i;
       const wipTagRegex = /(wip|do\s*not\s*merge|dnm)/i;
-      const isApollo = repositoryName == 'apollo';
-      const isMultipleCommit = commitNumber != '1';
+      const commitNumberLimit = 2;
+      const isNotCombinedCommit = commitNumber > commitNumberLimit;
       const isDevBranch = targetBranch == 'dev';
       const isWipTitle = wipTitleRegex.test(issueTitle);
       const isWipTaskList = container.querySelector('.timeline-comment') && container.querySelector('.timeline-comment').querySelector('input[type="checkbox"]:not(:checked)') !== null;
@@ -33,7 +33,7 @@
         isWipTag = isWipTag || label.textContent.match(wipTagRegex);
       }
 
-      disabled = (isWipTitle || isWipTaskList || isSquashCommits || isWipTag || (isDevBranch && isMultipleCommit && isApollo));
+      disabled = (isWipTitle || isWipTaskList || isSquashCommits || isWipTag || (isDevBranch && isNotCombinedCommit && isRelatedApollo));
 
       let buttonMessage = '';
 
